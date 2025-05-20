5003675e50712a7156a0798683278db5f6882eac
GodblessYankson <yanksongodbless06@gmail.com>
2025-05-19 23:04:05 -0700

    Still learning

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,10 +2,22 @@ import React, { useState } from 'react'
 
 const counter = () => {
     const [count, setCount] = useState(0)
+    const [movies, setMovies] = useState({
+        title: "DOOM",
+        rating: 7,
+        performance: 100
+})
+const handleClick = () => {
+    setMovies({...movies, rating: 10, performance: 88})
+}
   return (
     <div>
+        <p>{movies.title}</p>
+        <p>{movies.rating}</p>
+        <p>{movies.performance}</p>
+        <button onClick={handleClick} className='text-2xl text-red-500'>Click</button>
         counter = {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
+       
         </div>
   )
 }
